c6139250a538a6c7ed14f8b902d8f5feba6436dc
dongpu1813 <42663880+dongpu1813@users.noreply.github.com>
2020-10-14 22:17:48 +0800

    Update single.sh

--- single.sh
+++ single.sh
@@ -8,9 +8,9 @@ source $STORAGE_ROOT/yiimp/.yiimp.conf
 
 if [[ ! -e '$STORAGE_ROOT/yiimp/yiimp_setup/yiimp' ]]; then
 sudo rm -r $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
-hide_output sudo git clone https://github.com/cryptobuks1/yiimp-1 $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+hide_output sudo git clone https://github.com/yonnn/yiimp-4 $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 else
-hide_output sudo git clone https://github.com/cryptobuks1/yiimp-1 $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+hide_output sudo git clone https://github.com/yonnn/yiimp-4 $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
 fi
 
 echo Upgrading stratum...
