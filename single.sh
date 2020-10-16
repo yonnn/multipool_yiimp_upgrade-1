2caf424bfdb8759988ffebaa634395d73252cb07
dongpu1813 <42663880+dongpu1813@users.noreply.github.com>
2020-10-16 08:50:00 +0800

    Update single.sh

--- single.sh
+++ single.sh
@@ -1,5 +1,5 @@
 #####################################################
-# Created by bunbunbunbunbunny for crypto use...
+# Created by cryptopool.builders for crypto use...
 #####################################################
 
 source /etc/functions.sh
